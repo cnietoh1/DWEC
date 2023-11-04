a5523a2d97b7044a87e7466273a8f9443a965233
cnietoh1 <carlosnieth7@gmail.com>
2023-11-04 22:09:14 +0100

    version 04/11/2023 22:09

---
+++ EjerciciosJs/18.2/js/18.2.js
@@ -0,0 +1,5 @@
+console.log("Hola Mundo!");
+console.log("Soy el primer script")
+
+var mensaje = "Hola Mundo! Qué fácil es incluir 'comillas simples' y " + "\"" + "comillas dobles"  + "\""
+window.alert(mensaje)
---
+++ EjerciciosJs/18.3/js/18.3.js
@@ -0,0 +1,7 @@
+const meses = [
+    "Enero", "Febrero", "Marzo", "Abril", 
+    "Mayo", "Junio", "Julio", "Agosto",
+    "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+
+console.log(meses);
